fc7360951873e5067aba70ae4a9837297a068236
dana <danaw09@gmail.com>
2019-08-06 13:05:10 -0500

    added property with employee class

--- TrashColllector/TrashColllector/Models/Employee.cs
+++ TrashColllector/TrashColllector/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace TrashColllector.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
     }
 }
