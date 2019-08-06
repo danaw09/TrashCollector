a26712773e81884c277cc70b37592016f2bdbe24
dana <danaw09@gmail.com>
2019-08-06 13:04:17 -0500

    added more models along with property

--- TrashColllector/TrashColllector/Models/Customer.cs
+++ TrashColllector/TrashColllector/Models/Customer.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TrashColllector.Models
 {
     public class Customer
     {
+        [Required]
+        [Key]
+        public string UserId { get; set; }
+        public string phone { get; set; }
+        public string LastName { get; set; }
+        public string address { get; set; }
+        public string addressId { get; set; }
+        public string WeeklyPickUpDay { get; set; }
+        public string postalCode { get; set; }
+
+
     }
+   
 }
