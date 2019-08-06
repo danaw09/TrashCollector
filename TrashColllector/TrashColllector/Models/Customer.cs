8bafe366542b479f243887efa555cc65ff71374c
dana <danaw09@gmail.com>
2019-08-06 11:45:58 -0500

    inital test

---
+++ TrashColllector/TrashColllector/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashColllector.Models
+{
+    public class Customer
+    {
+    }
+}
---
+++ TrashColllector/TrashColllector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashColllector.Startup))]
+namespace TrashColllector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
