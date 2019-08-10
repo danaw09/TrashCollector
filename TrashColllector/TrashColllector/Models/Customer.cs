ac0292ba721f69fc2b3348c6513cfe56a2dd0bd6
dana <danaw09@gmail.com>
2019-08-09 23:06:34 -0500

    added display name in the customer class and also modified variables in the customer class

--- TrashColllector/TrashColllector/Models/Customer.cs
+++ TrashColllector/TrashColllector/Models/Customer.cs
@@ -6,19 +6,43 @@ namespace TrashColllector.Models
     {
         [Required]
         [Key]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "user id")]
         public string UserId { get; set; }
+
+        [Display(Name = "Phone")]
         public string phone { get; set; }
-        public string firstname { get; set; }
+        
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "street address")]
         public string Streetaddress { get; set; }
+
+        [Display(Name = "address Id")]
         public string AddressId { get; set; }
+
+        [Display(Name = "weekly pick up day")]
         public string WeeklyPickUpDay { get; set; }
+
+        [Display(Name = "Postal Code")]
         public string postalCode { get; set; }
+
+        [Display(Name = "Bill")]
         public string bill { get; set; }
+
+        [Display(Name = "CIty")]
         public string city { get; set; }
+
+        [Display(Name = "State")]
         public string state { get; set; }
+
+        [Display(Name = "Address")]
         public object Address { get; internal set; }
 
+        [Display(Name = "Weekly Pick Up Day Id")]
         public string WeeklyPickUpDayId { get; set; }
 
     }
